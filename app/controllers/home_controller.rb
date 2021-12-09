86fc4e0d6028c7270c7ec934ad691e8ac94c83ac
Dishant Sharma <dishant.sharma@w3villa.com>
2021-12-09 22:38:37 +0530

    blogo show page same as jump.africa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,12 @@
 class HomeController < ApplicationController
 
 	def index
-		# @posts = Blogo::Post.published.where(:is_custom => false).sort.reverse[0..15]
-		@latest_posts = Blogo::Post.published.where(:is_custom => false).sort.reverse.first(3)
+		@posts = Blogo::Post.published.where(is_custom: false).page(1).per(16)
+		@latest_posts = Blogo::Post.published.where(:is_custom => false).first(3)
 		@re = /<("[^"]*"|'[^']*'|[^'">])*>/
 		@meta_description = "The best place to find the current naira exchange rate today. Get live updates on the black market and learn how you can get the exchange rate in Nigeria today"
 		@meta_title = "Naira Exchange Rate Today | Trade Naira"
 
-		@posts = Blogo::Post.published.where(is_custom: false).page(1).per(16)
 
 	end
 
@@ -26,6 +25,8 @@ class HomeController < ApplicationController
 		@post = Blogo::Post.published.where(:permalink => params[:permalink], :is_custom => false).first!
 		@meta_description = @post.meta_description if @post.meta_description.present?
 		@meta_title = @post.meta_title.present? ? @post.meta_title : @post.title
+		@latest_posts = Blogo::Post.published.where(:is_custom => false).first(5)
+		@tags = Blogo::Tag.all
 	end
 
 	def search_tags
